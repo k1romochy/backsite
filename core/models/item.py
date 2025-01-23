4fef347354dce21cad7357fa41720d7e18029648
k1romochysch <tvinknene@mail.ru>
2025-01-23 14:48:20 +0300

    create item model, create relationship user with items o2m

---
+++ core/models/item.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from . import User
+from .base import Base
+
+
+class Item(Base):
+    name: Mapped[str] = mapped_column(nullable=False)
+    count: Mapped[int] = mapped_column(nullable=False)
+
+    user_id: Mapped[int] = mapped_column(nullable=False)
+    user: Mapped[User] = relationship('User', back_populates='items')
--- core/models/user.py
+++ core/models/user.py
@@ -3,9 +3,12 @@ from typing import TYPE_CHECKING
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from .base import Base
+from .item import Item
 
 
 class User(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True)
     role: Mapped[str] = mapped_column(server_default='User')
+
+    items: Mapped[list[Item]] = relationship('Item', back_populates='user')
