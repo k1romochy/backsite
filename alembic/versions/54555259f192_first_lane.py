d8069d623edf97155c5fad8c3afe52f72c768f6e
k1romochy@mail.ru@ <k1romochy@mail.ru>
2025-02-07 08:11:21 +0300

    gitignore vcs

---
+++ alembic/versions/54555259f192_first_lane.py
@@ -0,0 +1,26 @@
+"""First lane
+
+Revision ID: 54555259f192
+Revises: 
+Create Date: 2025-01-16 20:50:05.827659
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '54555259f192'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
